3a56e691eef99f820e4c754c76b52fd171f792e0
derek_cai <derek_cai@163.com>
2018-08-02 11:44:14 +0800

    react-router@4

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
-import { browserHistory, Router } from 'react-router';
 import './App.scss';
-import routes from "./Routes";
 
 
 export default class App extends React.Component<{}, {}> {
   public render(): any {
     return (
       <div className="App">
-        <Router history={browserHistory} routes={routes} />
+          {this.props.children}
       </div>
     );
   }
